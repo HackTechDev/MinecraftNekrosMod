ed5d8e0f8d36fcf931b81ac543541730df417264
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2015-09-10 20:01:41 +0200

    [Add][Mod] Armor

--- NekrosMod/src/main/java/com/bedrockminer/tutorial/Main.java
+++ NekrosMod/src/main/java/com/bedrockminer/tutorial/Main.java
@@ -11,8 +11,8 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = Main.MODID, name = Main.MODNAME, version = Main.VERSION)
 public class Main {
 
-	public static final String MODID = "Nekros";
-	public static final String MODNAME = "Nekros Mod";
+	public static final String MODID = "tutorial";
+	public static final String MODNAME = "Tutorial Mod";
 	public static final String VERSION = "1.0.0";
 
 	@Instance
--- NekrosMod/src/main/java/com/bedrockminer/tutorial/item/ModItems.java
+++ NekrosMod/src/main/java/com/bedrockminer/tutorial/item/ModItems.java
@@ -2,12 +2,15 @@ package com.bedrockminer.tutorial.item;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemArmor.ArmorMaterial;
 import net.minecraftforge.common.util.EnumHelper;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public final class ModItems {
 
-	public static ToolMaterial TUTORIAL = EnumHelper.addToolMaterial("TUTORIAL", 3, 1000, 15.0F, 4.0F, 30);
+	public static ToolMaterial TTUTORIAL = EnumHelper.addToolMaterial("TTUTORIAL", 3, 1000, 15.0F, 4.0F, 30);
+	public static ArmorMaterial ATUTORIAL = EnumHelper.addArmorMaterial("ATUTORIAL", 16, new int[] {3, 8, 6, 3}, 30);
+
 	public static Item tutorialPickaxe;
 	public static Item tutorialAxe;
 	public static Item tutorialSpade;
@@ -15,13 +18,23 @@ public final class ModItems {
 	public static Item tutorialSword;
 	public static Item tutorialMultiTool;
 
+	public static Item tutorialHelmet;
+	public static Item tutorialChestplate;
+	public static Item tutorialLeggings;
+	public static Item tutorialBoots;
+
 	public static final void init() {
-		GameRegistry.registerItem(tutorialPickaxe = new ItemModPickaxe("tutorial_pickaxe", TUTORIAL), "tutorial_pickaxe");
-		GameRegistry.registerItem(tutorialAxe = new ItemModAxe("tutorial_axe", TUTORIAL), "tutorial_axe");
-		GameRegistry.registerItem(tutorialSpade = new ItemModSpade("tutorial_spade", TUTORIAL), "tutorial_spade");
-		GameRegistry.registerItem(tutorialHoe = new ItemModHoe("tutorial_hoe", TUTORIAL), "tutorial_hoe");
-		GameRegistry.registerItem(tutorialSword = new ItemModSword("tutorial_sword", TUTORIAL), "tutorial_sword");
-		GameRegistry.registerItem(tutorialMultiTool = new ItemModMultitool("tutorial_multitool", TUTORIAL), "tutorial_multitool");
+		GameRegistry.registerItem(tutorialPickaxe = new ItemModPickaxe("tutorial_pickaxe", TTUTORIAL), "tutorial_pickaxe");
+		GameRegistry.registerItem(tutorialAxe = new ItemModAxe("tutorial_axe", TTUTORIAL), "tutorial_axe");
+		GameRegistry.registerItem(tutorialSpade = new ItemModSpade("tutorial_spade", TTUTORIAL), "tutorial_spade");
+		GameRegistry.registerItem(tutorialHoe = new ItemModHoe("tutorial_hoe", TTUTORIAL), "tutorial_hoe");
+		GameRegistry.registerItem(tutorialSword = new ItemModSword("tutorial_sword", TTUTORIAL), "tutorial_sword");
+		GameRegistry.registerItem(tutorialMultiTool = new ItemModMultitool("tutorial_multitool", TTUTORIAL), "tutorial_multitool");
+
+		GameRegistry.registerItem(tutorialHelmet = new ItemModArmor("tutorial_helmet", ATUTORIAL, "tutorial", 0), "tutorial_helmet");
+		GameRegistry.registerItem(tutorialChestplate = new ItemModArmor("tutorial_chestplate", ATUTORIAL, "tutorial", 1), "tutorial_chestplate");
+		GameRegistry.registerItem(tutorialLeggings = new ItemModArmor("tutorial_leggings", ATUTORIAL, "tutorial", 2), "tutorial_leggings");
+		GameRegistry.registerItem(tutorialBoots = new ItemModArmor("tutorial_boots", ATUTORIAL, "tutorial", 3), "tutorial_boots");
 	}
 
 }
