3f50f5c40cdcd26fac967f252be8be06f271f6ce
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2015-12-06 19:48:02 +0100

    Add recipes

--- NekrosMod/compilationMod.sh
+++ NekrosMod/compilationMod.sh
@@ -2,5 +2,5 @@
 
 ./gradlew build
 ls -l build/libs/
-cp build/libs/nekros-1.0.jar ~/.minecraft/mods
-cp build/libs/nekros-1.0.jar ~/JEUX/MINECRAFT/Serveur/mods
+cp build/libs/Nekros-1.0.jar ~/.minecraft/mods
+cp build/libs/Nekros-1.0.jar ~/JEUX/MINECRAFT/Serveur/mods
--- NekrosMod/src/main/java/com/bedrockminer/tutorial/Main.java
+++ NekrosMod/src/main/java/com/bedrockminer/tutorial/Main.java
@@ -8,6 +8,17 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
+import cpw.mods.fml.common.registry.EntityRegistry;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+import net.minecraft.block.BlockDispenser;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+
 @Mod(modid = Main.MODID, name = Main.MODNAME, version = Main.VERSION)
 public class Main {
 
@@ -26,6 +37,7 @@ public class Main {
 	public void preInitMod(FMLPreInitializationEvent event)
 	{
 		Entities.preinit();
+        addRecipes();
 	}
 	
 	@EventHandler
@@ -42,4 +54,42 @@ public class Main {
 	public void postInit(FMLPostInitializationEvent e) {
 		proxy.postInit(e);
 	}
+
+
+    private void addRecipes() 
+    {
+        // Recipes
+        // uses 1 item cookie and 7 block sponge in the pattern to make 1 diamond.
+        GameRegistry.addRecipe(new ItemStack(Items.diamond), 
+            "y y", 
+            "yxy", 
+            "yyy", 
+            'y', Blocks.sponge, 
+            'x', Items.cookie 
+        );
+        
+        //uses 4 iron in a the pattern to make 2 gold.(The same shape of a crafting bench put it must be in the top left corner of the grid)
+        GameRegistry.addRecipe(new ItemStack(Items.gold_ingot, 2), 
+            "xx ", 
+            "xx ", 
+            "   ", 
+            'x', Items.iron_ingot 
+        );
+        
+        //uses 8 black wool(1 being the count, 15 being the damage which makes it black) 1 gold block to make 1 diamond block 
+        GameRegistry.addRecipe(new ItemStack(Blocks.diamond_block), 
+            "yyy", 
+            "yxy", 
+            "yyy", 
+            'y', new ItemStack(Blocks.wool, 1, 15), 
+            'x', Blocks.gold_block
+        );
+        
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.diamond), Items.gold_ingot, Items.gold_ingot, Items.gold_ingot, Items.gold_ingot);//uses 4 gold bars placed anywhere in the crafting table to get 1 diamond(have to be placed separately not stacked on top of each other)
+        
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.diamond, 2), Items.gold_ingot, Items.gold_ingot, Items.gold_ingot, Items.gold_ingot, Items.diamond);// uses 4 gold and 1 diamond to give you 2 diamonds.
+        
+        GameRegistry.addShapelessRecipe(new ItemStack(Blocks.wool, 1 ,5), Blocks.wool, Items.stick);// uses 1 wool of any color and 1 stick to give you 1 lime wool. (1 being the count, 5 being the metadata making the wool lime colored).
+        
+    }
 }
